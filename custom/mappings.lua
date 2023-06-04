5448f0c05a88e3b54228ec254ff9c2527c1be936
ThalisRibeiro <thalis199@outlook.com>
2023-06-04 00:39:10 -0300

    Mappings do primegen com comentarios

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -8,5 +8,19 @@ M.general = {
 }
 
 -- more keybinds!
+M.theprime = {
+  n={
+    --Half Page Jumping centralizer
+    ["<C-d>"] = {"<C-d>zz", "Half page down"},
+    ["<C-u>"] = {"<C-u>zz", "Half page up"},
+    --Search centralizer
+    ["n"] = {"nzzzv", "Next result"},
+    ["N"] = {"Nzzzv", "Previous result"}
+  },
+  v={
+    ["J"] = {":m '>+1<CR>gv=gv", "Go up with selected text"},
+    ["K"] = {":m '<-2<CR>gv=gv", "Go down with selected text"}
+  }
+}
 
 return M
